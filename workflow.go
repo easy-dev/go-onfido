12f057ed788d42eb0a74c2057aa786e27d828b32
Mateusz Jendrzyn <mateusz.jendrzyn@acaisoft.com>
2023-01-24 10:57:27 +0100

    fetch workflow run by id

--- workflow.go
+++ workflow.go
@@ -30,3 +30,14 @@ func (c *Client) CreateWorkflowRun(ctx context.Context, a WorkflowRun) (*Workflo
 	_, err = c.do(ctx, req, &resp)
 	return &resp, err
 }
+
+func (c *Client) GetWorkflowRun(ctx context.Context, id string) (*Workflow, error) {
+	req, err := c.newRequest("GET", "/workflow_runs/"+id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp Workflow
+	_, err = c.do(ctx, req, &resp)
+	return &resp, err
+}
